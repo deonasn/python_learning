ee9f8e2a5c0d878950e1c58aac25f54baae3cedf
Deon Anoth <deonasn@gmail.com>
2025-01-09 13:13:45 -0600

    Refactoring example section for JSON.
    Refactored the code into 3 functions.
    Learning Refactoring.

--- practicing.py
+++ practicing.py
@@ -1,16 +1,48 @@
 # just for practicing while reading
 # mostly in-text examples
 
+from json import JSONDecodeError
 from pathlib import Path
 import json
 
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-path = Path('numbers.json')
+def entry_new_username(path):
+    """Prompts the user to enter a new username."""
+    username = input("Please enter your username: ").strip()
+    contents = json.dumps(username)
+    path.write_text(contents)
+    return username
 
-# contents = json.dumps(numbers)
-# path.write_text(contents)
+def get_stored_username(path):
+    """Extracts the stored username from the given JSON data file."""
+    global empty_file_status
+    if path.exists():
+        contents = path.read_text()
+        try:
+            username = json.loads(contents)
+        except JSONDecodeError:
+            print("The JSON data file is empty!")
+            username = entry_new_username(path)
+            print(f"We'll remember you when you come back, {username}!")
+            empty_file_status = True
+            return username
+        else:
+            return username
+    else:
+        return None
 
-contents = path.read_text()
-numbers = json.loads(contents)
+def greet_user():
+    """Greets the user."""
+    global empty_file_status
+    path = Path('username.json')
+    empty_file_status = False
+    username = get_stored_username(path)
+    if username:
+        if empty_file_status:
+            pass
+        else:
+            print(f"Welcome back, {username}!")
+    else:
+        username = entry_new_username(path)
+        print(f"We'll remember you when you come back, {username}!")
 
-print(numbers)
+greet_user()
