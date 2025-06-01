fd8a7c12e14fc7b4cc58deae1952ded7f7b0a521
Deon Anoth <deonasn@gmail.com>
2025-06-01 03:04:05 -0500

    Update practicing.py
    changed the empty default value of 'middle' from '' to None.

--- practicing.py
+++ practicing.py
@@ -1,10 +1,10 @@
 # just for practicing while reading
 # mostly in-text examples
 
-def get_formatted_name(first, last, middle=''):
+def get_formatted_name(first, last, middle=None):
     """Generate a neatly formatted full name."""
-    if middle:
-        full_name = f"{first} {middle} {last}"
-    else:
+    if middle == None:
         full_name = f"{first} {last}"
+    else:
+        full_name = f"{first} {middle} {last}"
     return full_name.title()
