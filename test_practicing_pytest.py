cd47a27d8680eada70576ce91770435940a829f0
Deon Anoth <deonasn@gmail.com>
2025-06-01 20:04:26 -0500

    Update test_practicing_pytest.py
    refactored for practicing pytest fixtures.

--- test_practicing_pytest.py
+++ test_practicing_pytest.py
@@ -2,12 +2,26 @@
 # imports functions from practicing.py
 # mostly in-text examples
 
+import pytest
 from practicing import AnonymousSurvey
 
-def test_store_print_multiple_response():
-    """Test that a single response is stored correctly."""
+@pytest.fixture
+def language_survey():
+    """Fixture to create an instance of AnonymousSurvey."""
     question = "What language did you first learn to speak?"
     language_survey = AnonymousSurvey(question)
+    return language_survey
+
+def test_store_single_response(language_survey):
+    """Test that a single response is stored correctly."""
+    response = 'English'
+    language_survey.store_response(response)
+    
+    expected_result = "\t\t\t- English\n\n"
+    assert language_survey.show_results() == expected_result
+
+def test_store_print_multiple_response(language_survey):
+    """Test that a single response is stored correctly."""
     responses = ['Arabic', 'Japanese', 'English', 'Korean', 'Chinese']
     for response in responses:
         language_survey.store_response(response)
